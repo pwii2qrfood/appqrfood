3c85ca7629a76ed0e8ec50c2e4b742510098fb79
Laisa <paiva.xavier.laisa@outlook.com>
2017-12-01 15:40:23 -0300

    Criação do UsuarioDAO e JpaUtil

--- com.anl/src/main/java/br/edu/ifrn/projeto/DAO/UsuarioDAO.java
+++ com.anl/src/main/java/br/edu/ifrn/projeto/DAO/UsuarioDAO.java
@@ -1,5 +1,44 @@
 package br.edu.ifrn.projeto.DAO;
 
-public class UsuarioDAO {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.edu.ifrn.projeto.model.Situacao;
+import br.edu.ifrn.projeto.model.Usuario;
+
+public class UsuarioDAO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private EntityManager manager;
+	
+	public UsuarioDAO() {}
+	
+	public UsuarioDAO(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	public void adicionar(Usuario novousuario) {
+		manager.persist(novousuario);
+	}
+	
+	public Usuario pesquisarPorId(int id) {
+		return manager.find(Usuario.class, id);
+	}
+	
+	public void remover(Usuario usuario) {
+		usuario = pesquisarPorId(usuario.getIdCliente());
+		manager.remove(usuario);
+	}
+	
+	public List<Usuario> pesquisar (String nome){
+		TypedQuery<Usuario> consulta =
+				manager.createQuery("from Usuario where nome like :nome_completo", Usuario.class);
+		consulta.setParameter("nome", nome + "%");
+		return consulta.getResultList();
+	}
 
 }
